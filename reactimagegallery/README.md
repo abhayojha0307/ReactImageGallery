26fdd1f8986fe3a25bb88089ed9ef45a38897195
Abhay Ojha <abhayojha0307@gmail.com>
2021-11-18 23:42:57 +0530

    Image galary using ReactJS and Tailwindcss

---
+++ reactimagegallery/README.md
@@ -0,0 +1,12 @@
+# Image Gallery using ReactJS, Tailwindcss.
+
+This project is build using ReactJS, Tailwindcss to view the image in a specific way. The Data is fetch from Pixabay. In this we can search image based on keyword.
+
+## Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open (http://localhost:3000) to view it in the browser.
---
+++ reactimagegallery/src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from "react";
+import ImageCard from "./component/ImageCard";
+import Search from "./component/Search"
+ 
+function App() {
+  const [images,setImages]=useState([]);
+  const [loading,setLoading]=useState(true);
+  const [keyword,setKeyword]=useState("")
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${keyword}&image_type=photo&pretty=true`)
+    .then(res=>res.json())
+    .then(data=>{
+      setImages(data.hits);
+      setLoading(false)
+    })
+    .catch(err=>console.log(err))
+  },[keyword]);
+
+
+  return (
+    <div className="container mx-auto">
+      <Search searchText={(text)=>setKeyword(text)} />
+      
+      {!loading && images.length===0 && <h1 className="test-6xl text-center mx-auto mt-32">No Images Found..</h1>}
+
+      {loading?<h1 className="test-6xl text-center mx-auto mt-32">Loading...</h1>:<div className="grid grid-cols-3 gap-4">
+        {images.map(image=>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
+    </div>
+  );
+}
+
+export default App;
